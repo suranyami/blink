08d69f207a47e0a94f320b53be35d055e8758048
David Parry <david.parry@suranyami.com>
2019-10-27 09:47:46 +1100

    First draft

---
+++ lib/blink.exs
@@ -0,0 +1,39 @@
+defmodule Blink do
+  @open_time 100
+  @closed_time 10
+  @beginning_of_line "\r"
+
+  def run do
+    IO.ANSI.clear()
+    |> IO.write()
+
+    eyes_open()
+  end
+
+  def eyes_open do
+    print(" OwO ")
+    time = @open_time * :rand.uniform(30)
+    :timer.sleep(time)
+    blink()
+    eyes_open()
+  end
+
+  def blink do
+    print(" ^_^ ")
+    time = @closed_time * :rand.uniform(30)
+    :timer.sleep(time)
+  end
+
+  defp print(message) do
+    (IO.ANSI.home() <> IO.ANSI.cursor_down(10))
+    |> IO.write()
+
+    IO.write(
+      @beginning_of_line <>
+        "                               " <>
+        message
+    )
+  end
+end
+
+Blink.run()
